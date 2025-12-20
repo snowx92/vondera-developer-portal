250807f4ede4bd13fef14be3f5de1b8b5aa69cb9
snowx92 <jonsnowelshreef92@gmail.com>
2025-12-20 06:29:01 +0200

    Add icons metadata to the RootLayout component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,9 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "Vondera Developer Portal",
   description: "Developer portal for Vondera",
+  icons: {
+    icon: "/logo.png",
+  },
 };
 
 export default function RootLayout({
